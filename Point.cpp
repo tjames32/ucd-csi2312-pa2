fea281cd6961b39b5df22f23d6ab9a7d96a3eeb9
tjames32 <tyler.james@ucdenver.edu>
2015-09-21 22:55:02 -0600

    Create Point.cpp

---
+++ Point.cpp
@@ -0,0 +1,62 @@
+#include "Point.h"
+#include <cmath>
+
+// Default constructor
+// Initializes the point to (0.0, 0.0, 0.0)
+Point::Point() {
+  x = 0.0;
+  y = 0.0;
+  z = 0.0;
+}
+
+// Constructor
+// Initializes the point to (initX, initY, initZ)
+Point::Point(double initX, double initY, initZ) {
+  x = initX;
+  y = initY;
+  z = initZ;
+}
+
+// Destructor
+// No dynamic allocation, so nothing to do; if omitted, generated automatically
+Point::~Point() {
+  // no-op
+}
+
+// Mutator methods
+// Change the values of private member variables
+
+void Point::setX(double newX) {
+  x = newX;
+}
+
+void Point::setY(double newY) {
+  y = newY;
+}
+
+void Point::setZ(double newZ){
+  z = newZ;
+}
+
+// Accessors
+// Return the current values of private member variables
+
+double Point::getX() {
+  return x;
+}
+
+double Point::getY() {
+  return y;
+}
+
+double Point::getZ(){
+  return z;
+}
+
+/*
+ * Function for calculating distance based off of square root of ((x * x) + (y * y) + (z * Z))
+ */
+double Point::distanceTo(Point &newPoint)
+{
+  return sqrt((x - newPoint.x) * (x - newPoint.x) + (y - nextPoint.y) * (y - newPoint.y) + (z - newPoint.z) * (z - newPoint.z));
+}
