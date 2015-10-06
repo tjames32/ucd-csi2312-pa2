b5102459f3ca17c269b5173ba4d3f1a2cb167c90
tjames32 <tyler.james@ucdenver.edu>
2015-10-05 20:06:04 -0600

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,37 +1,58 @@
-//
-// Created by Tyler on 9/21/2015.
-//
+#ifndef CLUSTERING_POINT_H
+#define CLUSTERING_POINT_H
 
-#ifndef CODE_POINT_H
-#define CODE_POINT_H
+#include <iostream>
 
-namespace;
+namespace Clustering {
 
-class Point
-{
-    private:
-        double dim;
+    class Point {
+        int dim;        // number of dimensions of the point
+        double *values; // values of the point's dimensions
 
     public:
+        Point(int);
+        Point(int, double *);
 
-        //Point();
+        // Big three: cpy ctor, overloaded operator=, dtor
+        Point(const Point &);
+        Point &operator=(const Point &);
+        ~Point();
 
-        double [] coordinates;
-        int dimensions;
-    
-        Point(int dim, double[] array)
-            {
-                 coordinates = new [dim];
-        
-                for(int i = 0; i < dim[]; i++)
-                coordinates[i] = array[i];
-            }
+        // Accessors & mutators
+        int getDims() const { return dim; }
+        void setValue(int, double);
+        double getValue(int) const;
 
-    double distanceTo(Point* nextPoint)
-        {
-            int dim = min(nextPoint->coordinates[i] - coordinates[i]);
-        }
+        // Functions
+        double distanceTo(const Point &) const;
 
-    
+        // Overloaded operators
 
-};
+        // Members
+        Point &operator*=(double);
+        Point &operator/=(double);
+        const Point operator*(double) const; // prevent (p1*2) = p2;
+        const Point operator/(double) const;
+
+        double &operator[](int index) { return values[index - 1]; } // TODO out-of-bds?
+
+        // Friends
+        friend Point &operator+=(Point &, const Point &);
+        friend Point &operator-=(Point &, const Point &);
+        friend const Point operator+(const Point &, const Point &);
+        friend const Point operator-(const Point &, const Point &);
+
+        friend bool operator==(const Point &, const Point &);
+        friend bool operator!=(const Point &, const Point &);
+
+        friend bool operator<(const Point &, const Point &);
+        friend bool operator>(const Point &, const Point &);
+        friend bool operator<=(const Point &, const Point &);
+        friend bool operator>=(const Point &, const Point &);
+
+        friend std::ostream &operator<<(std::ostream &, const Point &);
+        friend std::istream &operator>>(std::istream &, Point &);
+    };
+
+}
+#endif //CLUSTERING_POINT_H
