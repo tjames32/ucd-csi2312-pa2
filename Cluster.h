a7e34e368e35b4dc6ccd95e8cdb5841f34d65ecc
tjames32 <tyler.james@ucdenver.edu>
2015-10-05 20:07:09 -0600

    Create Cluster.h

---
+++ Cluster.h
@@ -0,0 +1,64 @@
+#ifndef CLUSTERING_CLUSTER_H
+#define CLUSTERING_CLUSTER_H
+
+#include "Point.h"
+
+namespace Clustering {
+
+    typedef Point *PointPtr;
+    typedef struct LNode *LNodePtr;
+
+//    struct LNode;
+//    typedef LNode *LNodePtr;
+
+    struct LNode {
+        PointPtr p;
+        LNodePtr next;
+    };
+
+    class Cluster {
+        int size;
+        LNodePtr points;
+
+    public:
+        Cluster() : size(0), points(nullptr) {};
+
+        // The big three: cpy ctor, overloaded operator=, dtor
+        Cluster(const Cluster &);
+        Cluster &operator=(const Cluster &);
+        ~Cluster();
+
+        // Set functions: They allow calling c1.add(c2.remove(p));
+        void add(const PointPtr &);
+        const PointPtr &remove(const PointPtr &);
+
+        // Overloaded operators
+
+        // IO
+        friend std::ostream &operator<<(std::ostream &, const Cluster &);
+        friend std::istream &operator>>(std::istream &, Cluster &);
+
+        // Set-preserving operators (do not duplicate points in the space)
+        // - Friends
+        friend bool operator==(const Cluster &lhs, const Cluster &rhs);
+
+        // - Members
+        Cluster &operator+=(const Cluster &rhs); // union
+        Cluster &operator-=(const Cluster &rhs); // (asymmetric) difference
+
+        Cluster &operator+=(const Point &rhs); // add point
+        Cluster &operator-=(const Point &rhs); // remove point
+
+        // Set-destructive operators (duplicate points in the space)
+        // - Friends
+        friend const Cluster operator+(const Cluster &lhs, const Cluster &rhs);
+        friend const Cluster operator-(const Cluster &lhs, const Cluster &rhs);
+
+        friend const Cluster operator+(const Cluster &lhs, const PointPtr &rhs);
+        friend const Cluster operator-(const Cluster &lhs, const PointPtr &rhs);
+
+    };
+
+}
+#endif //CLUSTERING_CLUSTER_H
+
